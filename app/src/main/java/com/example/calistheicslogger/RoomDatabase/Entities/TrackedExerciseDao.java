42e2fabf255d958b35a1b88d1343a9a45e9836e1
murock <alfiebpage@gmail.com>
2020-01-15 17:46:15 +0000

    Fixed personalRecordQuery

--- app/src/main/java/com/example/calistheicslogger/RoomDatabase/Entities/TrackedExerciseDao.java
+++ app/src/main/java/com/example/calistheicslogger/RoomDatabase/Entities/TrackedExerciseDao.java
@@ -32,29 +32,26 @@ public interface TrackedExerciseDao {
     //@Query("UPDATE tracked_exercises set set_number = -set_number where set_number =:setNo1 or set_number =:setNo2;")
     @Query("UPDATE tracked_exercises set set_number = (case set_number when :setNo1 then :setNo2 else :setNo1 end) where set_number in (:setNo1, :setNo2)")
     void swapBySetNumber(int setNo1, int setNo2);
-
-    @Query("SELECT * FROM tracked_exercises INNER JOIN (SELECT band, MAX(reps) AS Maxreps FROM tracked_exercises WHERE exercise_name =:name GROUP BY band ) topset ON tracked_exercises.band = topset.band AND tracked_exercises.reps = topset.Maxreps")
-    List<TrackedExercise> getPersonalRecords2(String name);
-
-    @Query("SELECT *, MAX(weight) weight  FROM tracked_exercises WHERE exercise_name =:name GROUP BY reps,band ")
+        
+    @Query("-- gets the max reps at each set of exercise_name, band, weight\n" +
+            "with repsCTE as (\n" +
+            "  select *,max(reps) reps\n" +
+            "  from tracked_exercises\n" +
+            "  where exercise_name = :name\n" +
+            "  group by exercise_name, band, weight ), \n" +
+            "\n" +
+            "-- gets the max weight at each set of exercise_name, band, reps\n" +
+            "weightCTE as (\n" +
+            "  select *,max(weight) weight\n" +
+            "  from tracked_exercises\n" +
+            "  where exercise_name = :name\n" +
+            "  group by exercise_name, band, reps ) \n" +
+            "\n" +
+            "select * \n" +
+            "from repsCTE r join weightCTE w\n" +
+            "on r.id = w.id\n" +
+            "order by band,reps" )
     List<TrackedExercise> getPersonalRecords(String name);
-
-//    @Query("with cte as (\n" +
-//            "  select t.id,t.exercise_name,t.timestamp,t.set_number,t.band, t.reps, t.weight,t.time,t.distance,t.tempo,t.angle\n" +
-//            "  from ( \n" +
-//            "    select *,\n"+      "" +
-//            "      row_number() over (partition by band order by reps desc) rnreps,\n" +
-//            "      row_number() over (partition by band, reps order by weight desc) rnweight" +
-//            "    from tracked_exercises\n" +
-//            "  ) t  \n" +
-//            "  where exercise_name = :name\n" +
-//            ")  \n" +
-//            "select * from cte c\n" +
-//            "where not exists (\n" +
-//            "  select 1 from cte\n" +
-//            "  where band = c.band and reps > c.reps and weight >= c.weight\n" +
-//            ")  \n")
-//    List<TrackedExercise> getPersonalRecords(String name);
 
     @Insert
     void addTrackedExercise(TrackedExercise trackedExercise);
