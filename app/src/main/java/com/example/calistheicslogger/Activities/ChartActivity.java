0d0d5f01e9946d24e3d07545739c2ae882111f63
murock <alfiebpage@gmail.com>
2020-02-03 18:17:54 +0000

    continue work on charts

--- app/src/main/java/com/example/calistheicslogger/Activities/ChartActivity.java
+++ app/src/main/java/com/example/calistheicslogger/Activities/ChartActivity.java
@@ -8,8 +8,13 @@ import androidx.annotation.Nullable;
 
 import com.example.calistheicslogger.R;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class ChartActivity extends Activity {
 
@@ -24,28 +29,63 @@ public class ChartActivity extends Activity {
     private void createGraph()
     {
         GraphView graph = (GraphView) findViewById(R.id.graph);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2020,1,1);
+        Date d1 = calendar.getTime();
+        calendar.set(2020,1,4);
+        Date d2 = calendar.getTime();
+        calendar.set(2020,1,10);
+        Date d3 = calendar.getTime();
+        calendar.set(2020,1,12);
+        Date d4 = calendar.getTime();
+        calendar.set(2020,1,15);
+        Date d5 = calendar.getTime();
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(d1, 1),
+                new DataPoint(d2, 2),
+                new DataPoint(d3, 4),
+                new DataPoint(d4, 5),
+                new DataPoint(d5, 9)
         });
-        //series.setBackgroundColor(Color.RED);
         series.setColor(Color.RED);
+        series.setTitle("tucked flag");
 
+        calendar.set(2020,1,17);
+        Date d6 = calendar.getTime();
+        calendar.set(2020,1,20);
+        Date d7 = calendar.getTime();
+        calendar.set(2020,1,27);
+        Date d8 = calendar.getTime();
         LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 4),
-                new DataPoint(1, 2),
-                new DataPoint(2, 5),
-                new DataPoint(3, 2),
-                new DataPoint(4, 1)
+                new DataPoint(d4, 1),
+                new DataPoint(d5, 1),
+                new DataPoint(d6, 2),
+                new DataPoint(d7, 2),
+                new DataPoint(d8, 4)
         });
-        //series.setBackgroundColor(Color.GREEN);
-        series.setColor(Color.GREEN);
+        series2.setColor(Color.GREEN);
+        series2.setTitle("full flag");
 
         graph.addSeries(series);
         graph.addSeries(series2);
-        
+        // Set up graph legend
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
+
+        // Set date label formatter
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
+
+        // set manual x bounds to have nice steps
+        graph.getViewport().setMinX(d1.getTime());
+        graph.getViewport().setMaxX(d8.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        // as we use dates as labels, the human rounding to nice readable numbers
+        // is not necessary
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
     }
+
+    public 
 }
