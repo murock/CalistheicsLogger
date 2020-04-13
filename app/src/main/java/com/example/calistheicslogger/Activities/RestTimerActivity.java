c24a067ff6d961322d4308723b27559463371cf7
murock <alfiebpage@gmail.com>
2020-04-13 09:10:51 +0100

    Updates to rest timer
    - textboxes will update if seconds enter over 59
    - arrow buttons hooked up

--- app/src/main/java/com/example/calistheicslogger/Activities/RestTimerActivity.java
+++ app/src/main/java/com/example/calistheicslogger/Activities/RestTimerActivity.java
@@ -55,17 +55,50 @@ public class RestTimerActivity extends Activity {
         getWindow().setLayout((int)(width*widthRatio),(int)(height*heightRatio));
     }
 
+
+
     private void SetUpTimerValue()
     {
         SharedPreferences prefs = getSharedPreferences("SharedPreferences", MODE_PRIVATE);
         timerValue = prefs.getInt("timerValue", 0); // 0 is default
         EditText secondsEditText = findViewById(R.id.secondsEditText);
         EditText minutesEditText = findViewById(R.id.minutesEditText);
-        secondsEditText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 99)});
+        secondsEditText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 999)});
         minutesEditText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 99)});
 
         int minutesValue = timerValue/60;
         int secondsValue = timerValue - (minutesValue*60);
+
+        minutesEditText.setText(Integer.toString(minutesValue));
+        minutesEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0)
+                {
+                    int minutes = Integer.parseInt(s.toString());
+                    int seconds = Integer.parseInt(secondsEditText.getText().toString());
+                    int totalTimeSeconds = minutes*60 + seconds;
+                    if (totalTimeSeconds == timerValue)
+                    {
+                        return;
+                    }
+
+                    timerValue = totalTimeSeconds;
+
+                    updateSharedPrefsTimerValue(totalTimeSeconds);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         secondsEditText.setText(Integer.toString(secondsValue));
         secondsEditText.addTextChangedListener(new TextWatcher() {
@@ -77,9 +110,32 @@ public class RestTimerActivity extends Activity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (s.length() > 0) {
-                    SharedPreferences.Editor editor = getSharedPreferences("SharedPreferences", MODE_PRIVATE).edit();
-                    editor.putInt("timerValue", Integer.parseInt(s.toString()));
-                    editor.apply();
+                    String minutesString = minutesEditText.getText().toString();
+                    int minutes = 0;
+                    if (minutesString.length() > 0)
+                    {
+                        minutes = Integer.parseInt(minutesEditText.getText().toString());
+                    }
+                    int seconds = Integer.parseInt(s.toString());
+                    int totalTimeSeconds = minutes*60 + seconds;
+                    if (totalTimeSeconds == timerValue)
+                    {
+                        return;
+                    }
+
+                    timerValue = totalTimeSeconds;
+                    int newMinutesValue = totalTimeSeconds/60;
+                    int newSecondsValue = totalTimeSeconds - (newMinutesValue*60);
+
+                    updateSharedPrefsTimerValue(totalTimeSeconds);
+
+                    if (!minutesEditText.getText().toString().equals(Integer.toString(newMinutesValue)))
+                    {
+                        minutesEditText.setText(Integer.toString(newMinutesValue));
+                    }
+                    if (!secondsEditText.getText().toString().equals(Integer.toString(newSecondsValue))){
+                        secondsEditText.setText(Integer.toString(newSecondsValue));
+                    }
                 }
             }
 
@@ -88,6 +144,13 @@ public class RestTimerActivity extends Activity {
 
             }
         });
+    }
+
+    private void updateSharedPrefsTimerValue(int newValue)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences("SharedPreferences", MODE_PRIVATE).edit();
+        editor.putInt("timerValue", newValue);
+        editor.apply();
     }
 
     private void SetUpCheckboxes()
@@ -135,15 +198,35 @@ public class RestTimerActivity extends Activity {
         });
     }
 
+    private void updateTimerValues()
+    {
+        EditText secondsEditText = findViewById(R.id.secondsEditText);
+        EditText minutesEditText = findViewById(R.id.minutesEditText);
+
+        int minutesValue = timerValue/60;
+        int secondsValue = timerValue - (minutesValue*60);
+
+        minutesEditText.setText(Integer.toString(minutesValue));
+        secondsEditText.setText(Integer.toString(secondsValue));
+    }
+
     public void OnSecondButtonPress(View button)
     {
-        int amountToChange;
         if (button.getId() == R.id.positiveButton)
         {
-            amountToChange = 10;
+            timerValue += 10;
         }else
         {
-            amountToChange = -10;
+            if (timerValue > 10){
+                timerValue -= 10;
+            }else
+            {
+                timerValue = 0;
+            }
         }
+        updateSharedPrefsTimerValue(timerValue);
+
+        updateTimerValues();
+
     }
 }
