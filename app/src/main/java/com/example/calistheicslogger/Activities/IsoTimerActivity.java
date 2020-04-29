c4d8ccbe0cbfba054cdfad5a094ca915ff152fdc
murock <alfiebpage@gmail.com>
2020-04-29 16:11:42 +0100

    Begin work on isometric timer

---
+++ app/src/main/java/com/example/calistheicslogger/Activities/IsoTimerActivity.java
@@ -0,0 +1,18 @@
+package com.example.calistheicslogger.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.calistheicslogger.R;
+
+public class IsoTimerActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.rest_timer_activity);
+    }
+}
